593c5e9ff0930007bc68e3bb8d4a1cf8eccd9c47
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-06-11 12:21:50 +0100

    Reordered setup.sh lines

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,7 +3,7 @@
 pushd ~
 
 ln -s dotfiles/toby-griffiths/.screenrc .
-ln -s dotfiles/toby-griffiths/.vimrc .
 ln -s dotfiles/toby-griffiths/.bashrc .
 ln -s dotfiles/toby-griffiths/.bash_profile .
 ln -s dotfiles/toby-griffiths/.bash_aliases .
+ln -s dotfiles/toby-griffiths/.vimrc .
