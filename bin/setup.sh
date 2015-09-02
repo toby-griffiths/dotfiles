8fd845241fc4f4aefedac0faf5a3068b15b05b29
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-09-02 14:01:01 +0100

    Added autocompletes to setup script
    And rearranged lines

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,8 +2,12 @@
 
 pushd ~
 
-ln -s dotfiles/toby-griffiths/.screenrc .
-ln -s dotfiles/toby-griffiths/.bashrc .
-ln -s dotfiles/toby-griffiths/.bash_profile .
+ln -s dotfiles/toby-griffiths/.autocompletes .
 ln -s dotfiles/toby-griffiths/.bash_aliases .
+ln -s dotfiles/toby-griffiths/.bash_profile .
+ln -s dotfiles/toby-griffiths/.bashrc .
+ln -s dotfiles/toby-griffiths/.gitignore .
+ln -s dotfiles/toby-griffiths/.screenrc .
 ln -s dotfiles/toby-griffiths/.vimrc .
+
+popd
