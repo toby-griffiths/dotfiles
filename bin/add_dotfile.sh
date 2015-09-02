0041b5e6dc10c05620c672ae12d2ecf933673333
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-09-02 14:05:27 +0100

    Removed unnecessary path prefixes

--- bin/add_dotfile.sh
+++ bin/add_dotfile.sh
@@ -8,8 +8,8 @@ fi
 
 pushd ~
 
-mv $1 ~/dotfiles/toby-griffiths/$1
-ln -s ~/dotfiles/toby-griffiths/$1 $1
+mv $1 dotfiles/toby-griffiths/$1
+ln -s dotfiles/toby-griffiths/$1 $1
 echo "ln -s dotfiles/toby-griffiths/$1 ." >> ~/dotfiles/toby-griffiths/bin/setup.sh
 
 popd
